cc4a3a0b29a5729934c758998805fdeb151ec079
suhasdhokane <suhasdhokane2017@gmail.com>
2022-09-09 17:36:21 +0530

    my initial commit

---
+++ First/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace First.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public string Index()
+        {
+            return "Hello good Moring";
+        }
+
+        public string Index1()
+        {
+            return "INDEX 1 Method";
+        }
+
+        public ActionResult Login()
+        {
+            return View();
+            // by deafult it will search view by login name
+            // because action method
+        }
+    }
+}
